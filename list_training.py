2fc19bc5027bf583d919f4cb3071857ab10c253e
Lm2004 <andvitbel@gmail.com>
2019-02-21 20:47:54 +0300

    list training

---
+++ list_training.py
@@ -0,0 +1,19 @@
+planets = [
+    'Mercury',
+    'Venus',
+    'Earth',
+    'Mars',
+    'Jupiter',
+    'Saturn',
+    'Uranus',
+    'Neptune'
+]
+planets.append("Pluto")
+print(planets)
+print(len(planets))
+print(planets.pop())
+print(planets)
+planets[5] = "Chery"
+print(planets)
+planets[5] = "Saturn"
+print(planets[3:-3])
