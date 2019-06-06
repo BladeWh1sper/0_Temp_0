7019287cc4658124aa2e923d4f6e94451c6c2e28
LM2004 <andvitbel@gmail.com>
2019-06-06 20:31:19 +0300

    That

--- 2019.py
+++
@@ -1,4 +0,0 @@
-N = input("Enter1: ")
-droid = input("Enter2: ")
-numbers = list(map(int, (droid.split())))
-print(numbers.pop(numbers.index(min(numbers))), min(numbers))
--- learning_python.py
+++
@@ -1,7 +0,0 @@
-user_number = input ("Enter your number: ")
-val = int(user_number)
-like = input("Second number: ")
-lite = int(like)
-def squared(x, y):
-    return x * y
-print("This is the end number: ", squared(val, lite))
